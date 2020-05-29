0e5e402c6df8e3ccdf0cc5586f3586168cc4177f
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-05-29 09:20:18 -0300

    Implementação teste "Retorna episode 6 da url" e teste "Retorna um array de numeros dos episódios" em desenvolvimento.

--- projeto-aula-ricky-morty/src/filtro-aula.js
+++ projeto-aula-ricky-morty/src/filtro-aula.js
@@ -6,6 +6,53 @@ function filterByStatus(personagens, status) {
   return personagens.filter(personagem => personagem.status === status);
 }
 
+function filterByGender(personagens, gender) {
+  return personagens.filter(personagem => personagem.gender === gender);
+}
+
+function getEpisodeFromURL(url) {
+  const urlSplited = url.split("/");
+  return urlSplited[urlSplited.length - 1];
+}
+
+function generateEpisodeList(character) {
+  return character.episode.map(url => getEpisodeFromURL(url))
+}
+
+function mapCharacterToEpisodes(episodes, character) {
+  const characterEpisodes = generateEpisodeList(character);
+
+  let newEpisodes = {...episodes};
+
+  characterEpisodes.map(episode => {
+    if (newEpisodes[episode]){
+      newEpisodes = {
+        ...newEpisodes,
+        [episode]: [...newEpisodes[episode],character]
+      };
+      return;
+    }
+
+    newEpisodes[episode] = [character];
+  });
+
+  return newEpisodes;
+}
+
+function filterByEpisode(personagens,episode ){
+  const episodios = personagens.reduce( (episodes, character) => {
+    return mapCharcterToEpisodes(episodes,character);
+  },{});
+  return episodios[episode]
+}
+
 module.exports = {
-  filterByStatus
+  filterByStatus,
+  filterByGender,
+  filterByEpisode,
+  getEpisodeFromURL,
+  generateEpisodeList,
+  mapCharacterToEpisodes,
+
+
 };
--- projeto-aula-ricky-morty/src/filtro-aula.spec.js
+++ projeto-aula-ricky-morty/src/filtro-aula.spec.js
@@ -23,4 +23,35 @@ describe("Filtro",() => {
 
   });
 
+  describe("Sexo", () => {
+    it("Retorna somente os homens", () => {
+      const response = filtro.filterByGender(data.results,"Male");
+      expect(response.length).toBe(15);
+    });
+
+    it("Retorna somente as mulheres", () => {
+      const response = filtro.filterByGender(data.results,"Female");
+      expect(response.length).toBe(4);
+    });
+
+  });
+
+  describe("Episódios", () => {
+    it("Retorna episode 6 da url", () => {
+      const url = "https://rickandmortyapi.com/api/episode/6";
+      expect(filtro.getEpisodeFromURL(url)).toBe("6");
+    });
+
+    it("Retorna um array de numeros dos episódios", () => {
+      const personagem = data.results[6];
+      const ricky = data.results[0];
+      const episodes = {
+        '10': [ricky],
+        '11': [ricky],
+      };
+      //expect(personagem.name).toBe("Abradolf Lincler");
+      const response = filtro.mapCharacterToEpisodes(episodes,personagem);
+      expect(response['10'].length).toBe(2);
+    })
+  });
 });
